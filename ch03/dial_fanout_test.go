1646f8464517c3ba9662f8097a3d1b0c604f1897
David Weber <djw612@gmail.com>
2023-05-20 14:36:42 -0400

    add dial fanout test

--- ch03/dial_fanout_test.go
+++ ch03/dial_fanout_test.go
@@ -3,44 +3,72 @@ package ch03
 import (
 	"context"
 	"net"
-	"syscall"
+	"sync"
 	"testing"
 	"time"
 )
 
 func TestDialContextCancelFanOut(t *testing.T) {
 
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(10*time.Second))
+	ctx, cancel := context.WithDeadline(
+    context.Background(), 
+    time.Now().Add(10*time.Second),
+  )
 
-	sync := make(chan struct{})
+  listener, err := net.Listen("tcp", "127.0.0.1:")
 
-	go func() {
-		defer func() { sync <- struct{}{} }()
+  if err != nil {
+    t.Fatal(err)
+  } 
 
-		var d net.Dialer
-		d.Control = func(_, _ string, _ syscall.RawConn) error {
-			time.Sleep(time.Second)
-			return nil
-		}
+  defer listener.Close()
 
-		conn, err := d.DialContext(ctx, "tcp", "10.0.0.1:80")
+  go func() {
+    conn, err := listener.Accept()
+    if err == nil {
+      conn.Close()
+    }
+  }()
 
-		if err != nil {
-			t.Log(err)
-			return
-		}
+  dial := func(ctx context.Context, address string, response chan int, id int, wg *sync.WaitGroup) {
+    defer wg.Done()
+    
+    var d net.Dialer
 
-		conn.Close()
+    c, err := d.DialContext(ctx, "tcp", address)
 
-		t.Error("connection did not time out")
-	}()
+    if err != nil {
+      return
+    }
 
-	cancel()
+    c.Close()
 
-	// wait for result from goroutine's defer
-	<-sync
+    select {
+      case <- ctx.Done():
+    case response <- id:
+    }
+  }
+	
+  res := make(chan int)
 
-	if ctx.Err() != context.Canceled {
-		t.Errorf("expected canceled context; actual: %q", ctx.Err())
-	}
+  var wg sync.WaitGroup
+
+  for i := 0; i < 10; i++ {
+    wg.Add(1)
+    go dial(ctx, listener.Addr().String(), res, i+1, &wg)
+  }
+
+  response := <-res
+
+  cancel()
+
+  wg.Wait()
+
+  close(res)
+
+  if ctx.Err() != context.Canceled {
+    t.Errorf("expected canceled context; actual: %s", ctx.Err())
+  }
+
+  t.Logf("dialer %d retrieved the resource", response)
 }
