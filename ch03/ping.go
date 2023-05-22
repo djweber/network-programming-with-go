e23533ce8848912961e1c3472f0a1220d11d6747
David Weber <djw612@gmail.com>
2023-05-22 11:30:10 -0400

    add pinger

---
+++ ch03/ping.go
@@ -0,0 +1,54 @@
+package ch03
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+)
+
+const defaultPingInterval = 30 * time.Second
+
+func Pinger(ctx context.Context, w io.Writer, reset <- chan time.Duration) {
+  var interval time.Duration
+
+  select {
+    case <-ctx.Done():
+      fmt.Println("done")
+    case interval = <-reset:
+      fmt.Println("reset")
+    default:
+  }
+
+  if interval <= 0 {
+    interval = defaultPingInterval
+  }
+
+  timer := time.NewTimer(interval)
+
+  defer func() {
+    if !timer.Stop() {
+      <-timer.C
+    }
+  }()
+
+  for {
+    select {
+    case <- ctx.Done():
+      return
+    case newInterval := <-reset:
+      if !timer.Stop() {
+        <-timer.C
+      }
+      if newInterval > 0 {
+        interval = newInterval
+      }
+    case <-timer.C:
+      if _,err := w.Write([]byte("ping")); err != nil {
+        return
+      }
+    }
+
+    _ = timer.Reset(interval)
+  }
+}
